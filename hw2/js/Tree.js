58664e0bc02d08a4d8c0ca370073acdf6e0d7780
Milena Belianovich <112034622+milenabel@users.noreply.github.com>
2022-09-08 20:32:00 -0600

    pos & lvls

--- hw2/js/Tree.js
+++ hw2/js/Tree.js
@@ -10,8 +10,8 @@ class Tree {
 
   constructor(json) {
     console.log(json)
-    let name = this.name
-    let parentNode = this.parent
+    let name = this.nodeName
+    let parentNode = this.parentName
   }
 
   /**
@@ -19,6 +19,53 @@ class Tree {
    */
   buildTree () {
     // note: in this function you will assign positions and levels by making calls to assignPosition() and assignLevel()
+    this.assignPosition(0,0)
+    this.assignLevel(0,0)
+
+    this.assignPosition(1,1)
+    this.assignLevel(1,0)  
+
+    this.assignPosition(2,1)
+    this.assignLevel(2,2)
+
+    this.assignPosition(3,2)
+    this.assignLevel(3,0)
+
+    this.assignPosition(4,2)
+    this.assignLevel(4,1)
+
+    this.assignPosition(5,2)
+    this.assignLevel(5,2)
+
+    this.assignPosition(6,2)
+    this.assignLevel(6,5)
+
+    this.assignPosition(7,2)
+    this.assignLevel(7,5)
+
+    this.assignPosition(8,3)
+    this.assignLevel(8,2)
+
+    this.assignPosition(9,3)
+    this.assignLevel(9,3)
+
+    this.assignPosition(10,3)
+    this.assignLevel(10,4)
+    
+    this.assignPosition(11,4)
+    this.assignLevel(11,4)
+
+    this.assignPosition(12,3)
+    this.assignLevel(12,5)
+
+    this.assignPosition(13,3)
+    this.assignLevel(13,6)
+
+    this.assignPosition(14,4)
+    this.assignLevel(14,6)
+
+    this.assignPosition(15,4)
+    this.assignLevel(15,7)
 
   }
 

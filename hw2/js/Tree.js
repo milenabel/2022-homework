606f807df74dfedd91274dded7476893590cffc4
Milena Belianovich <112034622+milenabel@users.noreply.github.com>
2022-09-11 20:13:50 -0600

    edits

--- hw2/js/Tree.js
+++ hw2/js/Tree.js
@@ -9,9 +9,30 @@ class Tree {
   //Add code that will create this list of `Node` objects based on the input. This is a good place to populate the `parentNode` field of the `Node` objects as well.
 
   constructor(json) {
-    console.log(json)
-    var name = this.nodeName  //let?
-    var parentNode = this.parentName
+    // creating an array of objects by extracting it from json file
+    let nodeObj = [];
+    json.forEach(node => {
+      console.log(node)
+      let newNodeObj = new Node(node.name, node.parent);
+      nodeObj.push(newNodeObj);
+    });
+    console.log(nodeObj);
+
+    // nodeObj.forEach(node => {
+    //   console.log(node);
+    //   const found = nodeObj.find(element => {element.name === node.parent});
+    //   console.log(found);
+    //   // let newNodeObj = new Node(node.name, node.parent);
+    //   // nodeObj.push(newNodeObj);
+    // });
+
+    // nodeObj.forEach(node => {
+    //   //console.log(node);
+    //   node.parentNode = nodeObj.find(element => {
+    //     element.name === node.parent;
+    //   });
+    //   //console.log(nodeObj);
+    // });
   }
 
   /**
@@ -19,53 +40,94 @@ class Tree {
    */
   buildTree () {
     // note: in this function you will assign positions and levels by making calls to assignPosition() and assignLevel()
-    this.assignPosition(0,0)
-    this.assignLevel(0,0)
+    // let children = [];
+    // nodeObj.Node
 
-    this.assignPosition(1,1)
-    this.assignLevel(1,0)  
+    // nodeObj.
+    // assign children 
 
-    this.assignPosition(2,1)
-    this.assignLevel(2,2)
+    // let rootNode;
 
-    this.assignPosition(3,2)
-    this.assignLevel(3,0)
+    // this.staticPos = 0;
+    // this.mapPosition = {};
 
-    this.assignPosition(4,2)
-    this.assignLevel(4,1)
+    // nodeObj.forEach( nodeI => {
+    //   if (nodeI.parentName === "root")
+    //     rootNode = nodeI;
+    //   nodeObj.forEach(nodeJ => {
+    //     if (nodeI.name === nodeJ.parentName)
+    //       nodeI.addchild(nodeJ);
+    //     if (nodeI.parentName === nodeJ.name)
+    //       nodeI.parentNode = nodeJ;
+    //   })
+    // })
+    // this.assignLevel(rootNode, 0);
+    // this.mapPosition[rootNode.level] = 0;
+    // this.assignPosition(rootNode, 0);
 
-    this.assignPosition(5,2)
-    this.assignLevel(5,2)
+    // console.log(nodeObj);
 
-    this.assignPosition(6,2)
-    this.assignLevel(6,5)
+    // let rootNode;
+    // let current, tempNode;
+    // let i, j;
+    // this.static_pos = 0;
+    // this.mapPositions = {}
+    // let l = this.listOfNodes.length;
+    // for(i = 0; i < l; i++){
+    //     current = this.listOfNodes[i];
+    //     if(current.parentName === "root")
+    //         rootNode = current;
+    //     for(j = 0; j < l; j++){
+    //         tempNode = this.listOfNodes[j];
+    //         if(current.name === tempNode.parentName)
+    //             current.addChild(tempNode);
+    //         if(current.parentName === tempNode.name)
+    //             current.parentNode = tempNode;
+    //     }
+    // }
+    // this.assignLevel(rootNode, 0);
+    // this.mapPositions[rootNode.level] = 0;
+    // this.assignPosition(rootNode, 0);
+    // console.log(this.listOfNodes);
 
-    this.assignPosition(7,2)
-    this.assignLevel(7,5)
 
-    this.assignPosition(8,3)
-    this.assignLevel(8,2)
+    // for (let i = 0; i < 1; i++) {
+    //   current = this.nodeObj[i];
+    //   if (current.parentName === "root")
+    //     rootNode = current;
+    //   for (let j = 0; j < this.nodeObj.length; j++){
+    //     tempNode = this.nodeObj[j];
+    //     if (current.name === tempNode.parentName){
+    //       current.addChild(tempNode);
+    //     }
+    //     if (current.parentName === tempNode.name){
+    //       current.parentNode = tempNode;
+    //     }
+    //   }
+    // }
 
-    this.assignPosition(9,3)
-    this.assignLevel(9,3)
+    // let rootNode;
+    // let current, tempNode;
+    // let i, j;
+    // this.static_pos = 0;
+    // this.mapPositions = {}
+    // let l = this.listOfNodes.length;
 
-    this.assignPosition(10,3)
-    this.assignLevel(10,4)
-    
-    this.assignPosition(11,4)
-    this.assignLevel(11,4)
+    // for(i = 0; i < l; i++){
+    //     current = this.listOfNodes[i];
 
-    this.assignPosition(12,3)
-    this.assignLevel(12,5)
+    //     if(current.parentName === "root")
+    //         rootNode = current;
 
-    this.assignPosition(13,3)
-    this.assignLevel(13,6)
+    //     for(j = 0; j < l; j++){
+    //         tempNode = this.listOfNodes[j];
+    //         if(current.name === tempNode.parentName)
+    //             current.addChild(tempNode);
 
-    this.assignPosition(14,4)
-    this.assignLevel(14,6)
-
-    this.assignPosition(15,4)
-    this.assignLevel(15,7)
+    //         if(current.parentName === tempNode.name)
+    //             current.parentNode = tempNode;
+    //     }
+        
 
   }
 
@@ -73,13 +135,33 @@ class Tree {
    * Recursive function that assign levels to each node
    */
   assignLevel (node, level) {
-
+    // let i;
+    // node.level = level;
+    // let l = node.children.length;
+    // for(i = 0; i < l; i++){
+    //     this.assignLevel(node.children[i], level + 1);
+    // }
   }
 
   /**
    * Recursive function that assign positions to each node
    */
   assignPosition (node, position) {
+    // this.staticPos = Math.max(this.staticPos, position);
+    // node.position = position;
+
+    // if(node.children.length > 0){
+    //     this.assignPosition(node.children[0], position);
+    // }
+
+    // for(let i = 1; i < node.children.length; i++){
+    //     if(this.staticPos > position){
+    //         this.assignPosition(node.children[i], this.static_pos + 1);
+    //     }
+    //     else{
+    //         this.assignPosition(node.children[i], position+1);
+    //     }
+    // }
     
   }
 

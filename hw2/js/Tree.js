ea16f9b224b624c90880df506aa5dbd12eff907a
Milena Belianovich <112034622+milenabel@users.noreply.github.com>
2022-09-12 02:25:45 -0600

    final version

--- hw2/js/Tree.js
+++ hw2/js/Tree.js
@@ -8,168 +8,104 @@ class Tree {
 
   //Add code that will create this list of `Node` objects based on the input. This is a good place to populate the `parentNode` field of the `Node` objects as well.
 
+
   constructor(json) {
-    // creating an array of objects by extracting it from json file
-    let nodeObj = [];
+  // creating an array of objects by extracting it from json file
+    this.nodeList = [];
     json.forEach(node => {
-      console.log(node)
-      let newNodeObj = new Node(node.name, node.parent);
-      nodeObj.push(newNodeObj);
+      //console.log(node)
+      let newObj = new Node(node.name, node.parent);
+      newObj.parentNode = this.nodeList.find(element => element.name == newObj.parentName);
+      this.nodeList.push(newObj);
     });
-    console.log(nodeObj);
-
-    // nodeObj.forEach(node => {
-    //   console.log(node);
-    //   const found = nodeObj.find(element => {element.name === node.parent});
-    //   console.log(found);
-    //   // let newNodeObj = new Node(node.name, node.parent);
-    //   // nodeObj.push(newNodeObj);
-    // });
-
-    // nodeObj.forEach(node => {
-    //   //console.log(node);
-    //   node.parentNode = nodeObj.find(element => {
-    //     element.name === node.parent;
-    //   });
-    //   //console.log(nodeObj);
-    // });
-  }
+    console.log(this.nodeList);
+  } 
 
   /**
-   * Assign other required attributes for the nodes.
+   * Function that builds a tree from a list of nodes with parent refs
    */
-  buildTree () {
+  buildTree() {
     // note: in this function you will assign positions and levels by making calls to assignPosition() and assignLevel()
-    // let children = [];
-    // nodeObj.Node
+    this.staticPos = 0;
+    this.mapPos = {};
+    
+    // assigning children 
+    this.nodeList.forEach(node => {
+      for (let i=0; i<this.nodeList.length; i++)
+        if (this.nodeList[i].parentName === node.name) 
+          node.addChild(this.nodeList[i]);
+    });
 
-    // nodeObj.
-    // assign children 
-
-    // let rootNode;
-
-    // this.staticPos = 0;
-    // this.mapPosition = {};
-
-    // nodeObj.forEach( nodeI => {
-    //   if (nodeI.parentName === "root")
-    //     rootNode = nodeI;
-    //   nodeObj.forEach(nodeJ => {
-    //     if (nodeI.name === nodeJ.parentName)
-    //       nodeI.addchild(nodeJ);
-    //     if (nodeI.parentName === nodeJ.name)
-    //       nodeI.parentNode = nodeJ;
-    //   })
-    // })
-    // this.assignLevel(rootNode, 0);
-    // this.mapPosition[rootNode.level] = 0;
-    // this.assignPosition(rootNode, 0);
-
-    // console.log(nodeObj);
-
-    // let rootNode;
-    // let current, tempNode;
-    // let i, j;
-    // this.static_pos = 0;
-    // this.mapPositions = {}
-    // let l = this.listOfNodes.length;
-    // for(i = 0; i < l; i++){
-    //     current = this.listOfNodes[i];
-    //     if(current.parentName === "root")
-    //         rootNode = current;
-    //     for(j = 0; j < l; j++){
-    //         tempNode = this.listOfNodes[j];
-    //         if(current.name === tempNode.parentName)
-    //             current.addChild(tempNode);
-    //         if(current.parentName === tempNode.name)
-    //             current.parentNode = tempNode;
-    //     }
-    // }
-    // this.assignLevel(rootNode, 0);
-    // this.mapPositions[rootNode.level] = 0;
-    // this.assignPosition(rootNode, 0);
-    // console.log(this.listOfNodes);
-
-
-    // for (let i = 0; i < 1; i++) {
-    //   current = this.nodeObj[i];
-    //   if (current.parentName === "root")
-    //     rootNode = current;
-    //   for (let j = 0; j < this.nodeObj.length; j++){
-    //     tempNode = this.nodeObj[j];
-    //     if (current.name === tempNode.parentName){
-    //       current.addChild(tempNode);
-    //     }
-    //     if (current.parentName === tempNode.name){
-    //       current.parentNode = tempNode;
-    //     }
-    //   }
-    // }
-
-    // let rootNode;
-    // let current, tempNode;
-    // let i, j;
-    // this.static_pos = 0;
-    // this.mapPositions = {}
-    // let l = this.listOfNodes.length;
-
-    // for(i = 0; i < l; i++){
-    //     current = this.listOfNodes[i];
-
-    //     if(current.parentName === "root")
-    //         rootNode = current;
-
-    //     for(j = 0; j < l; j++){
-    //         tempNode = this.listOfNodes[j];
-    //         if(current.name === tempNode.parentName)
-    //             current.addChild(tempNode);
-
-    //         if(current.parentName === tempNode.name)
-    //             current.parentNode = tempNode;
-    //     }
-        
-
+    this.assignLevel(this.nodeList[0], 0);
+    this.mapPos[this.nodeList[0].level] = 0;
+    this.assignPosition(this.nodeList[0], 0);
+    //console.log(this.nodeList);
   }
 
   /**
    * Recursive function that assign levels to each node
    */
   assignLevel (node, level) {
-    // let i;
-    // node.level = level;
-    // let l = node.children.length;
-    // for(i = 0; i < l; i++){
-    //     this.assignLevel(node.children[i], level + 1);
-    // }
+    node.level = level;
+    node.children.forEach( nodeN => {this.assignLevel(nodeN, level+1)});
   }
+
 
   /**
    * Recursive function that assign positions to each node
    */
   assignPosition (node, position) {
-    // this.staticPos = Math.max(this.staticPos, position);
-    // node.position = position;
+    this.staticPos = Math.max(this.staticPos, position);
+    node.position = position;
 
-    // if(node.children.length > 0){
-    //     this.assignPosition(node.children[0], position);
-    // }
+    if(node.children.length > 0){
+        this.assignPosition(node.children[0], position);
+    }
 
-    // for(let i = 1; i < node.children.length; i++){
-    //     if(this.staticPos > position){
-    //         this.assignPosition(node.children[i], this.static_pos + 1);
-    //     }
-    //     else{
-    //         this.assignPosition(node.children[i], position+1);
-    //     }
-    // }
-    
+    for(let i = 1; i < node.children.length; i++){
+        if(this.staticPos > position){
+            this.assignPosition(node.children[i], this.staticPos + 1);
+        }
+        else{
+            this.assignPosition(node.children[i], position + 1);
+        }
+    } 
   }
 
   /**
    * Function that renders the tree
    */
+   
   renderTree () {
 
+    let selection = d3.select("body"); //select the entire body of the page
+      selection.append("svg")
+        .attr("height", 1200)
+        .attr("width", 1200);
+      let svgBlock = d3.selectAll("svg");
+      let x_mult = 200, y_mult = 120, x_shift = 100, y_shift = 100;
+      
+    this.nodeList.forEach( node => {
+    node.children.forEach(nodec => {
+      svgBlock.append("line")
+      .attr("x1",x_mult*(node.level) + x_shift)
+      .attr("y1",y_mult*(node.position) + y_shift)
+      .attr("x2",x_mult*(nodec.level) + x_shift)
+      .attr("y2",y_mult*(nodec.position) + y_shift);
+    });
+    
+    let node_group = svgBlock.append("g").attr("class","nodeGroup")
+      .attr("transform","translate("+x_mult*node.level+","+y_mult*node.position+")");
+      node_group.append("circle")
+        .attr("r",50)
+        .attr("cx",100)
+        .attr("cy",100);
+      node_group.append("text")
+        .attr("class", "label")
+        .attr("dx",x_shift)
+        .attr("dy",y_shift)
+        .text(node.name);
+    });
   }
-
 }
+

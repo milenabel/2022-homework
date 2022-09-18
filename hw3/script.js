bdc35917970eecbb352a0ab2653a9e44ee7c89a3
Milena Belianovich <112034622+milenabel@users.noreply.github.com>
2022-09-17 21:49:45 -0600

    whatever

--- hw3/script.js
+++ hw3/script.js
@@ -2,7 +2,7 @@
 const CHART_WIDTH = 500;
 const CHART_HEIGHT = 250;
 const MARGIN = { left: 50, bottom: 20, top: 20, right: 20 };
-const ANIMATION_DUATION = 300;
+const ANIMATION_DURATION = 300;
 
 setup();
 
@@ -25,106 +25,186 @@ function update (data) {
 
   // ****** TODO ******
 
+  // sort data 
+  //data = data.sort((cases, deaths) => (+cases - +deaths))
+  // data = data.sort((cases, deaths) => cases.x - deaths.x)
+  // for (let d of data){
+  //   d.deaths = +d.deaths;
+  //   d.cases = +d.cases
+  // }
 
-  // Syntax for line generator.
-  // when updating the path for line chart, use the function as the input for 'd' attribute.
-  // https://github.com/d3/d3-shape/blob/main/README.md
+  // console.log(data)
 
+  // // doesn't work
+  // let yAllScale = d3
+  // .scaleLinear()
+  // .domain([0, d3.max(data, d => d['metric'])])
+  // .range([CHART_HEIGHT - MARGIN.top - MARGIN.bottom, 0]);
 
-  // const lineGenerator = d3.line()
-  //   .x(d => the x coordinate for a point of the line)
-  //   .y(d => the y coordinate for a point of the line);
+  // let xBarScale = d3
+  // .scaleBand()
+  // .domain(d3.map(data, d => d['date']))
+  // .range([0, CHART_WIDTH - MARGIN.right- MARGIN.left])
+  // .padding(0.5);
+  
+  // let xLineAreaScale = d3
+  //   .scalePoint()
+  //   .domain(d3.map(data, d => d['date']))
+  //   .range([MARGIN.left, CHART_WIDTH - MARGIN.right])
 
-  // Syntax for area generator.
-  // the area is bounded by upper and lower lines. So you can specify x0, x1, y0, y1 seperately. 
-  // Here, since the area chart will have upper and lower sharing the x coordinates, we can just use x(). 
-  // Similarly, use the function as the input for 'd' attribute. 
+  // let labelset = data.map(d => d.date);
+  let dataset = (document.getElementById('metric').value==="deaths" ? data.map(d => d.deaths) : data.map(d => d.cases));
+  let dataset2 = (document.getElementById('metric').value==="deaths" ? data.map(d => d.cases) : data.map(d => d.deaths));
 
-  // const areaGenerator = d3.area()
-  //   .x(d => the x coordinates for upper and lower lines, both x0 and x1)
-  //   .y1(d => the y coordinate for the upper line)
-  //   .y0(d=> the base line y coordinate for the area);
-
-
-  //Set up scatter plot x and y axis. 
-  //Since we are mapping death and case, we need new scales instead of the ones above. 
-  //Cases would be the horizontal axis, so we need to use width related constants.
-  //Deaths would be vertical axis, so that would need to use height related constants.
-
-  let xScale = d3
-    .scaleLinear()
-    .domain(0, d3.max(data, d => d.x))
+  let xBarScale = d3
+    .scaleBand()
+    .domain(data.map(d => d.date))
     .range([MARGIN.left, CHART_WIDTH - MARGIN.right])
-    .nice();
+    .padding(0.5);
 
-  let yScale = d3
-    .scaleLinear()
-    .domain(0, d3.max(data, d => d.y))
+  let xLineAreaScale = d3
+    .scalePoint()
+    .domain(data.map(d => d.date))
+    .range([MARGIN.left, CHART_WIDTH - MARGIN.right])
+
+  let yLineAreaBarScale = d3
+    .scalePoint()
+    .domain(d3.scaleLinear().domain([0, d3.max(dataset)]).nice().ticks(10))
+    .range([CHART_HEIGHT - MARGIN.top - MARGIN.bottom,0]);
+
+  let xScatterScale = d3
+    .scalePoint()
+    .domain(d3.scaleLinear().domain([0, d3.max(dataset2)]).nice().ticks(10))
+    .range([MARGIN.left, CHART_WIDTH - MARGIN.right])
+
+  let yScatterScale = d3
+    .scalePoint()
+    .domain(d3.scaleLinear().domain([0,d3.max(dataset)]).nice().ticks(10))
     .range([CHART_HEIGHT - MARGIN.bottom - MARGIN.top, 0])
-    .nice();
+
+  d3.select(`#x-barAxis`)
+    .attr('transform', `translate(0, ${CHART_HEIGHT - MARGIN.bottom})`)
+    .call(d3.axisBottom(xBarScale));
+  
+  d3.select(`#y-barAxis`)
+    .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`)
+    .call(d3.axisLeft(yLineAreaBarScale));
 
   d3.select('#x-lineAxis')
     .attr('transform', `translate(0,${CHART_HEIGHT - MARGIN.bottom})`)
-    .call(d3.axisBottom(xScale));
+    .call(d3.axisBottom(xLineAreaScale));
 
   d3.select('#y-lineAxis')
-    .call(d3.axisLeft(yScale))
+    .call(d3.axisLeft(yLineAreaBarScale))
     .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`);
 
   d3.select('#x-areaAxis')
     .attr('transform', `translate(0,${CHART_HEIGHT - MARGIN.bottom})`)
-    .call(d3.axisBottom(xScale));
+    .call(d3.axisBottom(xLineAreaScale));
 
   d3.select('#y-areaAxis')
-    .call(d3.axisLeft(yScale))
+    .call(d3.axisLeft(yLineAreaBarScale))
     .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`);
-
-  d3.select('#x-barAxis')
-    .attr('transform', `translate(0, ${CHART_HEIGHT - MARGIN.bottom})`)
-    .call(d3.axisBottom(xScale))
-
-  d3.select('#y-barAxis')
-    .call(d3.axisBottom(yScale))
-    .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`)
 
   d3.select('#x-scatterAxis')
     .attr('transform', `translate(0, ${CHART_HEIGHT - MARGIN.bottom})`)
-    .call(d3.axisBottom(xScale))
+    .call(d3.axisBottom(xScatterScale))
 
   d3.select('#y-scatterAxis')
-    .call(d3.axisBottom(yScale))
+    .call(d3.axisLeft(yScatterScale))
     .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`)
-    
+
+
+
+
+    // all the code lower does not work so I commented it out, but i tried...
+
+
+
+
+  // d3.select("#barChart")
+  //   .selectAll('g')
+  //   .data(data)
+  //   .enter()
+  //   .append("rect")
+  //   .attr("x",(data, i) => xBarScale(labelset[i]))
+  //   .attr("y",(data, i)=> CHART_HEIGHT - MARGIN.bottom-(CHART_HEIGHT / d3.max(dataset))*dataset[i])
+  //   .attr("width", d => 20)
+  //   .attr("height", (data, i)=> (CHART_HEIGHT / d3.max(dataset))*dataset[i])
+  //   .attr("opacity", 0.8);
 
   // const areaGenerator = d3
   //   .area()
-  //   .x((data) => xScale(data.x))
-  //   .y1((data) => yScale(data.y) + MARGIN.top)
+  //   .x((data) => xScale(data.deaths))
+  //   .y1((data) => yScale(data.cases) + MARGIN.top)
   //   .y0(CHART_HEIGHT - MARGIN.bottom);
 
   // const lineGenerator = d3
   //   .line()
-  //   .x((data) => xScale(data.x))
-  //   .y((data) => yScale(data.y) + MARGIN.top);
+  //   .x((data) => xScale(data.deaths))
+  //   .y((data) => yScale(data.cases) + MARGIN.top);
+    
+
+  // const areaGenerator = d3
+  //   // .select('#AreaChart')
+  //   // .select('g')
+  //   .area()
+  //   .x((data, i) => xLineAreaScale(data[i].date))
+  //   .y1((dataset, i) => yLineAreaBarScale(dataset[i]) + MARGIN.top)
+  //   .y0(CHART_HEIGHT - MARGIN.bottom);
+
+  // const lineGenerator = d3
+  //   // .select('#LineChart')
+  //   // .select('g')
+  //   .line()
+  //   .x((data, i) => xLineAreaScale(data[i].date))
+  //   .y((dataset, i) => yLineAreaBarScale(dataset[i]) + MARGIN.top);
 
   // d3.select('#AreaChart')
   //   .select('path')
   //   .datum(data)
   //   .transition(ANIMATION_DURATION)
-  //   .attr('d', this.areaGenerator());
+  //   .attr('d', this.areaGenerator(data))
+  //   .attr('opacity', 1);
 
   // d3.select('#LineChart')
   //   .select('path')
   //   .datum(data)
   //   .transition(ANIMATION_DURATION)
-  //   .attr('d', this.lineGenerator());
-  
+  //   .attr('d', this.lineGenerator(data))
+  //   .attr('opacity', 1);
+
+  // d3.select('#BarChart')
+  //   .selectAll('g')
+  //   //.selectAll("rect")
+  //   .data(data)
+  //   .enter()
+  //   .append('rect')
+  //   .attr("x", (data, i)=> xBarScale(i))
+  //   .attr("y",(dataset, i)=> yLineAreaBarScale(i))
+  //   .attr("width", d => 10)
+  //   .attr("height", (dataset, i)=> yLineAreaBarScale(i))
+  //   .attr("opacity", 1);
+
+  // d3.select("#scatterplot")
+  //   .selectAll("circle")
+  //   .data(data)
+  //   .enter()
+  //   .append("circle")
+  //   .attr("cx", d => xScatterScale(d.cases))
+  //   .attr("cy", d => yScatterScale(d.deaths))
+  //   .attr("r", 5)
+    // .exit()
+    // .remove()
+
+  // d3.select('#BarChart')
+  //   .select('')
   //TODO 
   // call each update function below, adjust the input for the functions if you need to.
-  this.updateBarChart();
-  this.updateLineChart();
-  this.updateAreaChart();
-  this.updateScatterPlot();
+  // this.updateBarChart();
+  // this.updateLineChart();
+  // this.updateAreaChart();
+  // this.updateScatterPlot();
 }
 
 /**
@@ -132,13 +212,32 @@ function update (data) {
  */
 
 function updateBarChart () {
+  // d3.select('#BarChart')
+  //   .selectAll('g')
+  //   //.selectAll("rect")
+  //   .exit()
+  //   .remove()
+  //   .attr("x",(data, i) => xBarScale(labelset[i]))
+  //   .attr("y",(data, i)=> CHART_HEIGHT - MARGIN.bottom-(CHART_HEIGHT / d3.max(dataset))*dataset[i])
+  //   .attr("width", d => 20)
+  //   .attr("height", (data, i)=> (CHART_HEIGHT / d3.max(dataset))*dataset[i])
+  //   .attr("opacity", 0.8);
 
+  
 }
 
 /**
  * Update the line chart
  */
 function updateLineChart () {
+  // d3.select('#LineChart')
+  //   .exit()
+  //   .remove()
+  //   .select('path')
+  //   .datum(data)
+  //   .transition(ANIMATION_DURATION)
+  //   .attr('d', this.lineGenerator(data))
+  //   .attr('opacity', 1);
 
 }
 
@@ -146,8 +245,14 @@ function updateLineChart () {
  * Update the area chart 
  */
 function updateAreaChart () {
-
-
+  // d3.select('#AreaChart')
+  //   .exit()
+  //   .remove()
+  //   .select('path')
+  //   .datum(data)
+  //   .transition(ANIMATION_DURATION)
+  //   .attr('d', this.areaGenerator(data))
+  //   .attr('opacity', 1);
 }
 
 /**
@@ -155,7 +260,16 @@ function updateAreaChart () {
  */
 
 function updateScatterPlot () {
-
+  // d3.select("#scatterplot")
+  //   .exit()
+  //   .remove()
+  //   .selectAll("circle")
+  //   .data(data)
+  //   .enter()
+  //   .append("circle")
+  //   .attr("cx", d => xScatterScale(d.cases))
+  //   .attr("cy", d => yScatterScale(d.deaths))
+  //   .attr("r", 5)
 }
 
 
